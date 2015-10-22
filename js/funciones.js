ee84723a766cb6ee2d575fe745abc0e5edc66ff6
Inti Maldonado <imaldonado@trabajando.com>
2015-10-22 10:34:40 -0300

    Nuevo sitio lutheria maldonado

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+<!--funciones para fancybox PORTADA-->
+
+		$(document).ready(function() {
+				$("a#proyecto1").fancybox();
+				$("a#proyecto2").fancybox();
+				$("a#proyecto3").fancybox();
+				$("a#proyecto4").fancybox();
+				$("a#proyecto5").fancybox();
+				$("a#proyecto6").fancybox();
+				$("a#proyecto7").fancybox();
+				$("a#proyecto8").fancybox();
+				$("a#proyecto9").fancybox();
+				$("a#proyecto10").fancybox();
+				$("a#proyecto11").fancybox();
+				$("a#proyecto12").fancybox();
+				$("a#proyecto13").fancybox();
+				$("a#proyecto14").fancybox();
+				$("a#proyecto15").fancybox();
+				$("a#proyecto16").fancybox();
+		});
+		
+<!--funciones para NIVO SLIDER-->		
+		$(window).load(function() {
+        $('#slider').nivoSlider();
+    	});
+		
