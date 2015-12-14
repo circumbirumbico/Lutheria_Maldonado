c30880aa6ece22899c1a6d2b6a4caef49554715b
Carlos Sanchez <carlos.sanchez@trabajando.com>
2015-12-14 16:24:00 -0300

    completo secciones home

--- wp_theme/header.php
+++ wp_theme/header.php
@@ -11,4 +11,6 @@
 </head>
 <body>
 
-<header></header>
+<header>
+	<div class="container"></div>
+</header>
--- wp_theme/index.php
+++ wp_theme/index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<!-- omar-maldonado -->
 <section id="omar-maldonado">
 	<div class="container">
 		<div class="row">
@@ -18,7 +19,7 @@
 	</div>
 </section>
 
-
+<!-- galeria -->
 <section id="galeria">
 	<h2>Galería</h2>
 	<div class="container-fluid">
@@ -56,22 +57,51 @@
 	</div>
 </section>
 
-
+<!-- inicios -->
 <section id="inicios">
 	<h2>Inicios en la Luthería</h2>
 	<p>Mis inicios en este oficio de construir instrumentos (lutheria) data de cuando tenía cerca de nueve años de edad; mi padre en aquel entonces me regaló una guitarra que él construyó, y aparte de entusiasmarme en la ejecución de dicho instrumento me entusiasmó en la posibilidad de construirlos.</p>
 	<button>Leer más</button>
 </section>
 
+<!-- luthier -->
 <section id="luthier">
 	<h2>Omar Maldonado</h2>
-	<p>Patrocina Consejo Nacional de la Cultura y Las Artes Fondo Nacional de Desarrollo Cultural y Artes FONDART Gobierno de Chile</p>
+	<p>Patrocina <strong>Consejo Nacional de la Cultura y Las Artes</strong> Fondo Nacional de Desarrollo Cultural y Artes <strong>FONDART</strong> <strong>Gobierno de Chile</strong></p>
 	<button>Ver video</button>
 </section>
 
+<!-- testimonio -->
 <section id="testimonio">
 	<p>“LA LUTHERÍA SATISFACE PLENAMENTE MI ESPÍRITU Y MI INTENCIÓN DE PODER ENSEÑAR A AQUELLOS QUE DESEEN APRENDER ÉSTE OFICIO TAN HERMOSO”</p>
 </section>
 
+<!-- footer -->
+<footer id="contacto">
+	<div class="sobre">
+		<img src="<?php bloginfo('template_directory') ?>/img/contacto-lutheria.png" alt="Contacto Luthería">
+	</div>
+
+	<h2><strong>Contacto</strong> <?php bloginfo('name') ?></h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5 col-sm-offset-1">
+				<input type="text" placeholder="Nombre">
+				<input type="text" placeholder="Email">
+				<input type="text" placeholder="Asunto" class="asunto">
+			</div>
+
+			<div class="col-sm-5">
+				<textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+				<input type="submit" placeholder="Asunto" class="asunto">
+			</div>
+		</div>
+
+		<div class="copyright">
+			© 2015 LUTHERÍA MALDONADO | Todos los derechos reservados.
+		</div>
+
+	</div>
+</footer>
 	
 <?php get_footer(); ?>
